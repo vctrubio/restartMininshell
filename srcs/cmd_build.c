a9528dbf8ce2be6add977a3a923bd3286d65c7b3
vctrubio <vctrubio@gmail.com>
2022-10-30 16:41:28 +0000

    commit before teh change

--- srcs/cmd_build.c
+++ srcs/cmd_build.c
@@ -46,9 +46,11 @@ t_cmd	*init_tcmd(char **matrix)
 	cmd->args = malloc((1+ft_matrix_get_num_col(matrix))*sizeof(char**));
 	while (*(++matrix) != NULL)
 	{
-		// printf("%s args... \n", *matrix);
 		if (!is_redir(**matrix)) // | >> < << >
+		{
+			printf("%s args... \n", *matrix);
 			cmd->args[i++] = ft_strdup(*(matrix));
+		}
 		else
 		{
 			set_redir(cmd, &matrix); //*[ls][-la][>][file.txt]
@@ -68,24 +70,21 @@ void	build_cmds(char **matrix)
 	
 	printf("ft_build_cmd\n");
 	ptr = init_tcmd(matrix);
-	// print_tcmd(ptr);
 	g_envp.head = ptr;
 	*(matrix) = ptr->current_cmd_pos;
 	while (*(matrix) != NULL)
 	{
-		
 		// printf("in whileloop %s \n", *(matrix));
 		ptr_next = init_tcmd(matrix);
 		ptr->next = ptr_next;
 		ptr_next->prev = ptr;
 		ptr = ptr->next;
-		if (ptr_next->current_cmd_pos == NULL)
+		if (ptr->current_cmd_pos == NULL)
 			break;
 		else
 			*(matrix) = ptr->current_cmd_pos;
 	}
 	print_tcmd(g_envp.head);
-	//we return head
 	//ls
 	//1 cmd
 
@@ -96,27 +95,3 @@ void	build_cmds(char **matrix)
 	//2 cmd
 
 }
-
-/* EXAMPLE 1: ls -la > text.txt
-t_cmd	*cmd(malloc)
-
-cmd->cmd = ls
-cmd->args = -la
-cmd->type = R_OUT
-cmd->next = NULL
-
-t_file	*file(malloc)
-cmd->next = cmd2;
-
-
-// EXAMPLE 2: pwd|ls
-t_cmd	*t(malloc)
-t->cmd	=pwd
-t->args =NULL;
-t->type = PIPE
-t->next = t2
-
-t_cmd	*t2(malloc)
-t->cmd	=ls
-
-*/
