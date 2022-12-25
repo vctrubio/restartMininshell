834e3e2620f991389019845572814b814525454a
vctrubio <43044561+vctrubio@users.noreply.github.com>
2022-12-25 14:47:07 +0000

    good, neeeds testing

--- srcs/loop.c
+++ srcs/loop.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 15:17:04 by vrubio            #+#    #+#             */
-/*   Updated: 2022/12/25 14:39:49 by codespace        ###   ########.fr       */
+/*   Updated: 2022/12/25 14:46:46 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,12 +73,12 @@ void	loop_execution(t_cmd *cmd)
 	pid_t	pid;
 	int		status_bs[2];
 	char	*path;
-	int		wpd[2];
+	// int		wpd[2];
 	
 	status_bs[1] = 0;
 	printf("printing loop...\n");
 	print_tcmd(_shell()->head);
-	pipe(wpd);
+	// pipe(wpd); //idk what this does
 	while (cmd && cmd->args[0])
 	{
 		if (loop_part1(&cmd, &path))
